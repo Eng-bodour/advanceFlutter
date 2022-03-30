3e3b829e400e02f87ed2f8b9f395c11ab8e7d953
bodour majadmi <bodour.eng.28@gmail.com>
2022-03-30 18:03:22 +0300

    finish add route manager

--- lib/presentation/resources/routes_manager.dart
+++ lib/presentation/resources/routes_manager.dart
@@ -1,3 +1,12 @@
+import 'package:advance_flutter/presentation/forgot_password/forgot_password_view.dart';
+import 'package:advance_flutter/presentation/login/login_view.dart';
+import 'package:advance_flutter/presentation/main/main_view.dart';
+import 'package:advance_flutter/presentation/register/register_view.dart';
+import 'package:advance_flutter/presentation/splash/splash_view.dart';
+import 'package:advance_flutter/presentation/store_details/store_details_view.dart';
+
+import 'package:flutter/material.dart';
+
 class Routes {
   static const String splashRoute = '/';
   static const String loginRoute = '/loginRoute';
@@ -5,4 +14,42 @@ class Routes {
   static const String forgotPasswordRoute = '/forgotPasswordRoute';
   static const String mainRoute = '/mainRoute';
   static const String storeDetailsRoute = '/storeDetailsRoute';
+}
+
+class RouteGenerator {
+  //
+  //get Route
+  //
+  static Route<dynamic> getRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.splashRoute:
+        return MaterialPageRoute(builder: (_) => const SplashView());
+      case Routes.loginRoute:
+        return MaterialPageRoute(builder: (_) => const LogInView());
+      case Routes.registerRoute:
+        return MaterialPageRoute(builder: (_) => const RegisterView());
+      case Routes.forgotPasswordRoute:
+        return MaterialPageRoute(builder: (_) => const ForgotPasswordView());
+      case Routes.mainRoute:
+        return MaterialPageRoute(builder: (_) => const MainView());
+      case Routes.storeDetailsRoute:
+        return MaterialPageRoute(builder: (_) => const StoreDetailsView());
+      default:
+        return unDefnedRoute();
+    }
+  }
+
+  static Route<dynamic> unDefnedRoute() {
+    return MaterialPageRoute(
+        builder: (_) => Scaffold(
+              appBar: AppBar(
+                title: const Text(
+                    'No Route Found'), // to do move this Strings manager
+              ),
+              body: const Center(
+                child:
+                    Text('No Route Found'), // to do move this Strings manager
+              ),
+            ));
+  }
 }
