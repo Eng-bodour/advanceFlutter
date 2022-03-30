82de6e8ccf087d7131674f8091dffae6a92f4154
bodour majadmi <bodour.eng.28@gmail.com>
2022-03-30 15:55:58 +0300

    app bar theme

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,4 +1,6 @@
 import 'package:advance_flutter/presentation/color_manager.dart';
+import 'package:advance_flutter/presentation/font_manager.dart';
+import 'package:advance_flutter/presentation/styles_manager.dart';
 import 'package:advance_flutter/presentation/values_manager.dart';
 import 'package:flutter/material.dart';
 
@@ -10,6 +12,7 @@ ThemeData getApplicationTheme() {
       primaryColorLight: ColorManager.lightPrimary,
       primaryColorDark: ColorManager.darkPrimary,
       disabledColor: ColorManager.grey1,
+      splashColor: ColorManager.lightGray,
 
       //Cardview theme
 
@@ -17,9 +20,18 @@ ThemeData getApplicationTheme() {
         color: ColorManager.white,
         shadowColor: ColorManager.gray,
         elevation: AppSize.s4,
-      )
+      ),
 
       //appbar theme
+      appBarTheme: AppBarTheme(
+          color: ColorManager.primary,
+          centerTitle: true,
+          elevation: AppSize.s4,
+          shadowColor: ColorManager.lightPrimary,
+          titleTextStyle: getReqularStyle(
+            fontSize: FontSize.s16,
+            color: ColorManager.white,
+          ))
 
       //button theme
 
