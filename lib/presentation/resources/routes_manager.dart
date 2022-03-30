5cbc6d912efa6846f564f626c4c61d162fa0239a
bodour majadmi <bodour.eng.28@gmail.com>
2022-03-30 17:38:36 +0300

    route manager

---
+++ lib/presentation/resources/routes_manager.dart
@@ -0,0 +1,8 @@
+class Routes {
+  static const String splashRoute = '/';
+  static const String loginRoute = '/loginRoute';
+  static const String registerRoute = '/registerRoute';
+  static const String forgotPasswordRoute = '/forgotPasswordRoute';
+  static const String mainRoute = '/mainRoute';
+  static const String storeDetailsRoute = '/storeDetailsRoute';
+}
